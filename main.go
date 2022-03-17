c0fea588e8c17863f47554113dacf5dec00a9137
Beatrice Federici <beatrice.federici123@gmail.com>
2022-03-17 01:14:34 +0000

    learnt about for loops and if statements

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import "fmt"
+import "strings"
 
 func main(){
 
@@ -10,36 +11,61 @@ const conferenceTickets uint = 50
 var remainingTickets uint = 50
 
 fmt.Printf("Welcome to our %v booking application!\n", conferenceName)
-fmt.Printf(" Get your tickets here to attend the conference")
+fmt.Printf(" Get your tickets here to attend the conference\n")
 fmt.Printf("We have %v tickets and  %v remaining available tickets\n", conferenceTickets, remainingTickets)
 
 
-var userName string
-var userTickets uint
-var email string
+
+for{
+	var userName string
+	var userTickets uint
+	var email string
 
 
-//ask user for their name
-fmt.Println("Enter your first name")
-fmt.Scan(&userName)
+	//ask user for their name
+	fmt.Println("Enter your first name")
+	fmt.Scan(&userName)
 
-fmt.Println("Enter the number of tickets")
-fmt.Scan(&userTickets)
+	fmt.Println("Enter the number of tickets")
+	fmt.Scan(&userTickets)
+
+	if userTickets > remainingTickets {
+		fmt.Printf("We only have %v tickets remaining!", remainingTickets)
+		break
+	}
+
+	fmt.Println("Enter your email address")
+	fmt.Scan(&email)
 
 
-fmt.Println("Enter your email address")
-fmt.Scan(&email)
+	remainingTickets = remainingTickets - userTickets
+	fmt.Printf("Thank you %v  for booking %v tickets. You will recieve a confirmation email at %v \n", userName, userTickets, email)
 
 
-remainingTickets = remainingTickets - userTickets
-fmt.Printf("Thank you %v  for booking %v tickets. You will recieve a confirmation email at %v \n", userName, userTickets, email)
+	//arrays number defines how long the array is, the type ext to it defines what type the array will contain. arrays in go cannot mix and match types.
+	var bookings[] string
+
+	bookings = append(bookings, userName, email) //adding elements to a list
+
+	fmt.Printf("The whole array %v \n", bookings)
+	fmt.Printf("%v tickets remaining for %v\n", remainingTickets, conferenceName)
 
 
-//arrays number defines how long the array is, the type ext to it defines what type the array will contain. arrays in go cannot mix and match types.
-var bookings[50] string
+	firstNames := []string{}
+	for _, booking:= range bookings{
+		var names = strings.Fields(booking) // splits the string ith the white space as a separator, and returns a slice with split elements
+		var firstName = names[0]
+		firstNames = append(firstNames, firstName)
+	}
+	fmt.Printf("These are all our bookings %v\bn", bookings)
+	fmt.Printf("The first names of bookings are %v \n", firstNames)
 
-bookings[0] = userName
+	if remainingTickets == 0{
+		fmt.Printf( "We have run out of tickets, come back next year!")
+		break
+	}
+}
 
-fmt.Printf("The whole array %v \n", bookings)
+
 
 }
